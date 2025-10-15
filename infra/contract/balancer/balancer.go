11f1c624fda948b52c8b35409b9e63830502f663
crazyfrank <axu9417@gmail.com>
2025-10-15 20:51:58 +0800

    fix:remove balancer code

--- infra/contract/balancer/balancer.go
+++
@@ -1,9 +0,0 @@
-package balancer
-
-import "context"
-
-type Balancer interface {
-	Next(ctx context.Context) (string, error)
-	Update(services []string)
-	SetHealthCheck(checkFn func(addr string) bool)
-}
